c3bd26179226025f06fab47778b16dae476ff1b8
yashfun <71376380+yashfun@users.noreply.github.com>
2023-02-17 14:50:55 +0400

    Add files via upload

---
+++ assets/OutOfOrderView-0cb996e2.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c,a as e}from"./index-a121f2a5.js";const s={},n={class:"out-of-order container"},r=e("div",null,[e("h3",null,"Сервис временно недоступен"),e("h4",null,"Ведутся технические работы")],-1),_=[r];function a(l,d){return t(),c("section",n,_)}const f=o(s,[["render",a]]);export{f as default};
